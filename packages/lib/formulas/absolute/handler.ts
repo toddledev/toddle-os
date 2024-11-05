25c9e595b9cb26814b24bb86a6928bd621997e4c
Erik Beuschau <bager.erik@gmail.com>
2024-11-05 15:47:42 +0100

    Bump

--- packages/lib/formulas/absolute/handler.ts
+++
@@ -1,9 +0,0 @@
-const handler = ([a]: any[]) => {
-  if (typeof a !== "number") {
-    // throw new Error('Argument must be a number')
-    return null;
-  }
-  return Math.abs(a);
-};
-
-export default handler;
--- packages/lib/formulas/boolean/handler.ts
+++
@@ -1,7 +0,0 @@
-import { toBoolean } from "@toddle/core/src/util";
-
-const handler = ([input]: any[]) => {
-  return toBoolean(input);
-};
-
-export default handler;
