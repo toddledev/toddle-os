4a37ea7bbb576d1dcc7d23a660d3956093d6b0c7
Erik Beuschau <bager.erik@gmail.com>
2024-12-03 13:37:24 +0100

    Import parse from cookie package correctly

--- packages/ssr/src/rendering/cookies.ts
+++ packages/ssr/src/rendering/cookies.ts
@@ -1,9 +1,9 @@
 import { isDefined } from '@toddledev/core/dist/utils/util'
-import cookie from 'cookie'
+import { parse } from 'cookie'
 
 export const getRequestCookies = (req: Request) =>
   Object.fromEntries(
-    Object.entries(cookie.parse(req.headers.get('cookie') ?? '') ?? {}).filter(
+    Object.entries(parse(req.headers.get('cookie') ?? '')).filter(
       // Ensure that both key and value are defined
       (kv): kv is [string, string] => isDefined(kv[0]) && isDefined(kv[1]),
     ),
