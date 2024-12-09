63d1095ee14d1fa8ca4123dd8bac13112d186d71
Erik Beuschau <bager.erik@gmail.com>
2024-12-09 14:33:42 +0100

    Add logic for rendering the page body/components
    - Add `renderComponent` and `createComponent` functions
    - Add helper `renderPageBody` function that initializes the API cache and renders the page body
    - Add types for an API evaluator callback function
    - Update hono example with reset stylesheet + body rendering

--- examples/hono/src/routes/toddlePage.ts
+++ examples/hono/src/routes/toddlePage.ts
@@ -1,4 +1,13 @@
-import { applyFormula } from '@toddledev/core/dist/formula/formula'
+import { ToddleComponent } from '@toddledev/core/dist/component/ToddleComponent'
+import {
+  applyFormula,
+  type ToddleServerEnv,
+} from '@toddledev/core/dist/formula/formula'
+import { createStylesheet } from '@toddledev/core/dist/styling/style.css'
+import { theme as defaultTheme } from '@toddledev/core/dist/styling/theme.const'
+import { isDefined } from '@toddledev/core/dist/utils/util'
+import { takeIncludedComponents } from '@toddledev/ssr/dist/components'
+import { renderPageBody } from '@toddledev/ssr/dist/rendering/components'
 import { getPageFormulaContext } from '@toddledev/ssr/dist/rendering/formulaContext'
 import { getHtmlLanguage } from '@toddledev/ssr/dist/rendering/html'
 import {
@@ -6,7 +15,7 @@ import {
   matchPageForUrl,
 } from '@toddledev/ssr/dist/routing/routing'
 import type { Context } from 'hono'
-import { html } from 'hono/html'
+import { html, raw } from 'hono/html'
 import type { HonoEnv } from '../../hono'
 
 export const toddlePage = async (c: Context<HonoEnv>) => {
@@ -31,6 +40,59 @@ export const toddlePage = async (c: Context<HonoEnv>) => {
     formulaContext,
     defaultLanguage: 'en',
   })
+
+  // Find the theme to use for the page
+  const theme =
+    (c.env.project.files.themes
+      ? Object.values(c.env.project.files.themes)[0]
+      : c.env.project.files.config?.theme) ?? defaultTheme
+
+  // Get all included components on the page
+  const includedComponents = takeIncludedComponents({
+    root: page,
+    projectComponents: c.env.project.files.components,
+    packages: c.env.project.files.packages,
+    includeRoot: true,
+  })
+
+  // Currently, styles are inlined, but we want to serve these from a separate endpoint
+  const styles = createStylesheet(page, includedComponents, theme, {
+    includeResetStyle: false,
+    // Font faces are created from a stylesheet referenced in the head
+    createFontFaces: false,
+  })
+  const toddleComponent = new ToddleComponent<string>({
+    component: page,
+    getComponent: (name, packageName) => {
+      const nodeLookupKey = [packageName, name].filter(isDefined).join('/')
+      const component = packageName
+        ? c.env.project.files.packages?.[packageName]?.components[name]
+        : c.env.project.files.components[name]
+      if (!component) {
+        console.warn(`Unable to find component ${nodeLookupKey} in files`)
+        return undefined
+      }
+
+      return component
+    },
+    packageName: undefined,
+    globalFormulas: {
+      formulas: c.env.project.files.formulas,
+      packages: c.env.project.files.packages,
+    },
+  })
+  const { html: body } = await renderPageBody({
+    component: toddleComponent as any, // TODO: Fix typing
+    formulaContext,
+    env: formulaContext.env as ToddleServerEnv,
+    req: c.req.raw,
+    files: c.env.project.files,
+    includedComponents,
+    evaluateComponentApis: async (_) => ({
+      // TODO: Show an example of how to evaluate APIs - potentially using an adapter
+    }),
+    projectId: 'my_project',
+  })
   return c.html(
     html`<!doctype html>
       <html lang="${language}">
@@ -40,9 +102,18 @@ export const toddlePage = async (c: Context<HonoEnv>) => {
             ${applyFormula(page.route.info?.title?.formula, formulaContext) ??
             page.name}
           </title>
+          <!-- The rest of the <head> elements are coming soon 🤞 -->
+          <link
+            rel="stylesheet"
+            fetchpriority="high"
+            href="/_static/reset.css"
+          />
+          <style>
+            ${styles}
+          </style>
         </head>
         <body>
-          <h1>${page.name}</h1>
+          ${raw(body)}
         </body>
       </html>`,
   )
